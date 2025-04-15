b9ecae6d34e034f061ce75f3f293117e59e11a83
jzh <11297332+jiaozh1@user.noreply.gitee.com>
2025-04-15 15:09:07 +0800

    vue project init

--- app/module/bar/controller/utils.ts
+++ app/module/bar/controller/utils.ts
@@ -175,54 +175,120 @@ export class UtilsController {
       {
         title: "0-0",
         key: "0-0",
+        level: 1,
         children: [
           {
             title: "0-0-0",
             key: "0-0-0",
+            level: 0,
             children: [
-              { title: "0-0-0-0", key: "0-0-0-0" },
-              { title: "0-0-0-1", key: "0-0-0-1" },
-              { title: "0-0-0-2", key: "0-0-0-2" },
+              {
+                title: "0-0-0-0",
+                key: "0-0-0-0",
+                level: -1,
+              },
+              {
+                title: "0-0-0-1",
+                key: "0-0-0-1",
+                level: -1,
+              },
+              {
+                title: "0-0-0-2",
+                key: "0-0-0-2",
+                level: -1,
+              },
             ],
           },
           {
             title: "0-0-1",
             key: "0-0-1",
+            level: 0,
             children: [
-              { title: "0-0-1-0", key: "0-0-1-0" },
-              { title: "0-0-1-1", key: "0-0-1-1" },
-              { title: "0-0-1-2", key: "0-0-1-2" },
+              {
+                title: "0-0-1-0",
+                key: "0-0-1-0",
+                level: -1,
+              },
+              {
+                title: "0-0-1-1",
+                key: "0-0-1-1",
+                level: -1,
+              },
+              {
+                title: "0-0-1-2",
+                key: "0-0-1-2",
+                level: -1,
+              },
             ],
           },
-          { title: "0-0-2", key: "0-0-2" },
+          {
+            title: "0-0-2",
+            key: "0-0-2",
+            level: 0,
+          },
         ],
       },
       {
         title: "0-1",
         key: "0-1",
+        level: 1,
         children: [
           {
             title: "0-1-0",
             key: "0-1-0",
+            level: 0,
             children: [
-              { title: "0-1-0-0", key: "0-1-0-0" },
-              { title: "0-1-0-1", key: "0-1-0-1" },
-              { title: "0-1-0-2", key: "0-1-0-2" },
+              {
+                title: "0-1-0-0",
+                key: "0-1-0-0",
+                level: -1,
+              },
+              {
+                title: "0-1-0-1",
+                key: "0-1-0-1",
+                level: -1,
+              },
+              {
+                title: "0-1-0-2",
+                key: "0-1-0-2",
+                level: -1,
+              },
             ],
           },
           {
             title: "0-1-1",
             key: "0-1-1",
+            level: 0,
             children: [
-              { title: "0-1-1-0", key: "0-1-1-0" },
-              { title: "0-1-1-1", key: "0-1-1-1" },
-              { title: "0-1-1-2", key: "0-1-1-2" },
+              {
+                title: "0-1-1-0",
+                key: "0-1-1-0",
+                level: -1,
+              },
+              {
+                title: "0-1-1-1",
+                key: "0-1-1-1",
+                level: -1,
+              },
+              {
+                title: "0-1-1-2",
+                key: "0-1-1-2",
+                level: -1,
+              },
             ],
           },
-          { title: "0-1-2", key: "0-1-2" },
+          {
+            title: "0-1-2",
+            key: "0-1-2",
+            level: 0,
+          },
         ],
       },
-      { title: "0-2", key: "0-2" },
+      {
+        title: "0-2",
+        key: "0-2",
+        level: 1,
+      },
     ];
     this.helper.success({ res });
   }
